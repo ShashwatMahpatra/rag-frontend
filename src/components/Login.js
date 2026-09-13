735443a9a61fe3b36372438c2d5e577381ac7e17
ShashwatMahapatra <shashwat_mahapatra@yahoo.com>
2026-09-13 18:59:54 +0530

    Changed to the original url

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Login.css';
 
-const API_URL = process.env.REACT_KEY || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 console.log('API URL:', API_URL);
 
 function Login({ onLogin }) {
